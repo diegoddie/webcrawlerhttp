49b10e5dbad08852e38cfa593d35b7396fd508e5
Diego <diego.boost@gmail.com>
2024-11-14 21:51:36 +0100

    normalizeURL and tests

--- crawl.test.js
+++ crawl.test.js
@@ -2,11 +2,34 @@
 import normalizeUrl from './crawl';
 import { text, expect } from '@jest/globals';
 
-
-test("normalizeUrl", () => {
-    const input = ""
+test("normalizeUrl strip protocol", () => {
+    const input = "https://blog.boot.dev/path"
     const actual = normalizeUrl(input)
-    const expected = ""
+    const expected = "blog.boot.dev/path"
+
+    expect(actual).toEqual(expected)
+})
+
+test("normalizeUrl strip trailing slash", () => {
+    const input = "https://blog.boot.dev/path/"
+    const actual = normalizeUrl(input)
+    const expected = "blog.boot.dev/path"
+
+    expect(actual).toEqual(expected)
+})
+
+test("normalizeUrl capitals", () => {
+    const input = "https://BLOG.boot.dev/path/"
+    const actual = normalizeUrl(input)
+    const expected = "blog.boot.dev/path"
+
+    expect(actual).toEqual(expected)
+})
+
+test("normalizeUrl strip http", () => {
+    const input = "http://blog.boot.dev/path/"
+    const actual = normalizeUrl(input)
+    const expected = "blog.boot.dev/path"
 
     expect(actual).toEqual(expected)
 })
